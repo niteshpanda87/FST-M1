0006f90e921bf59701f22ba7ad257a73a47aa8ff
niteshpanda87 <nitesh.panda@ibm.com>
2024-06-13 12:27:54 +0530

    Add files via upload

---
+++ Python/Activities/Acitivit1.py
@@ -0,0 +1,5 @@
+name = input("Enter your Name: ")
+age = int (input("Enter your Age: "))
+year = str((2024 - age) + 100)
+
+print(f"Hi {name}, on {year} you will be turning 100 Year old.")
---
+++ Python/Activities/Activity2.py
@@ -0,0 +1,8 @@
+num = int (input("Enter a Number: "))
+
+mod = num % 2
+
+if mod == 0:
+    print(f"Entered number {num} is a Even number.20")
+else:
+    print(f"Entered number {num} is a Odd number.")
