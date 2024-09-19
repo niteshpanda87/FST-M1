14bd9431ffcddf4f484b71343e0a4cc325e3894c
niteshpanda87 <nitesh.panda@ibm.com>
2024-09-19 11:18:30 +0530

    Add files via upload

--- TestNG/Activities/Activity7.java
+++ TestNG/Activities/Activity7.java
@@ -1,46 +1,64 @@
-package Project;
+package Activity;
 
-import org.testng.annotations.Test;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-import org.testng.annotations.BeforeClass;
-
-import java.util.List;
+import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Activity7 {
-	WebDriver driver;
-	
-  @BeforeClass
-  public void setUp() {
-	  WebDriverManager.firefoxdriver().setup();
-	  driver = new FirefoxDriver(); 
-	  
-	  driver.get("https://alchemy.hguy.co/lms");
-  }
-  
-  @Test
-  public void allCourses() {
-	  // Locate All courses and click
-	  driver.findElement(By.id("menu-item-1508")).click();
-	  
-	  // Find All Courses on the Page
-	  int allCourses = driver.findElements(By.className("col-sm-8")).size();
-	  
-	  // Print the Number of Courses on the page
-	  System.out.println("Total Number of the Courses on the page is: " +allCourses);
-  }
+    WebDriver driver;
+    WebDriverWait wait;
 
-  @AfterClass
-  public void afterClass() {
-	  driver.quit();
-  }
+    @BeforeClass
+    public void beforeClass() {
+        // Set up the Firefox driver
+        WebDriverManager.firefoxdriver().setup();
+        // Initialize Firefox driver object
+        driver = new FirefoxDriver();
+        // Initialize wait object
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        
+        //Open browser
+        driver.get("https://v1.training-support.net/selenium/login-form");
+    }
+    
+    @DataProvider(name = "Authentication")
+    public static Object[][] credentials() {
+        return new Object[][] { { "admin", "password" }};
+    }
+    
+    @Test (dataProvider = "Authentication")
+    public void loginTestCase(String username, String password) {
+        //Find username and pasword fields
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        
+        //Enter values
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
+        
+        //Click Log in
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+        
+        //Assert Message
+        String loginMessage = driver.findElement(By.id("action-confirmation")).getText();
+        Assert.assertEquals(loginMessage, "Welcome Back, admin");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        //Close browser
+        driver.close();
+    }
 
 }
