4bca24153d5d31c4f0d4683818e6050096f02c02
niteshpanda87 <nitesh.panda@ibm.com>
2024-09-19 11:14:57 +0530

    Add files via upload

--- TestNG/Activities/Activity6.java
+++ TestNG/Activities/Activity6.java
@@ -1,62 +1,60 @@
-package Project;
+package Activity;
 
 import org.testng.annotations.Test;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 
 public class Activity6 {
-	WebDriver driver;
-	
-  @BeforeClass
-  public void setUp() {
-	  WebDriverManager.firefoxdriver().setup();
-	  driver = new FirefoxDriver(); 
-	  
-	  driver.get("https://alchemy.hguy.co/lms");
-  }
-  
-  @Test
-  public void myAccount() {
-	  driver.findElement(By.id("menu-item-1507")).click();
-	  
-	  // Locate the My Account Link and click on it
-	  String myAccountPageVerify = driver.findElement(By.xpath("//*[@id='uagb-infobox-d9258720-3455-443e-b618-b57211636d84']/div/div/div/div[1]/h1")).getText();
-	  System.out.println(myAccountPageVerify);
-	  Assert.assertEquals("My Account", myAccountPageVerify);
-  }
-  
-  @Test (dependsOnMethods = {"myAccount"})
-  public void logIn() {
-	  
-	  // Locate the Login Button and click on It
-	  driver.findElement(By.xpath("//*[@id=\"uagb-column-e9d225cb-cee9-4e02-a12d-073d5f051e91\"]/div[2]/div[2]/a")).click();
-	  
-	  // Locate the Username Box and enter username
-	  driver.findElement(By.id("user_login")).sendKeys("root");
-	  
-	  // Locate the Password box and enter password 
-	  driver.findElement(By.id("user_pass")).sendKeys("pa$$w0rd");
-	  
-	  // Click on the Login Button
-	  driver.findElement(By.id("wp-submit")).click();
-	  
-	  // Verify You have login sucessfully
-	  String loginSuccessMessage = driver.findElement(By.xpath("//*[@id=\"uagb-infobox-d9258720-3455-443e-b618-b57211636d84\"]/div/div/div/div[1]/h1")).getText();
-	  
-	  System.out.println(loginSuccessMessage);
-	  Assert.assertEquals("My Account", loginSuccessMessage);
-  }
+    WebDriver driver;
+    WebDriverWait wait;
 
-  @AfterClass
-  public void afterClass() {
-	  driver.quit();
-  }
+    @BeforeClass
+    public void beforeClass() {
+        // Set up the Firefox driver
+        WebDriverManager.firefoxdriver().setup();
+        // Initialize the Firefox driver
+        driver = new FirefoxDriver();
+        // Initialize the wait object
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        
+        //Open browser
+        driver.get("https://v1.training-support.net/selenium/login-form");
+    }
+    
+    @Test
+    @Parameters({"username", "password"})
+    public void loginTestCase(String username, String password) {
+        //Find username and pasword fields
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        
+        //Enter values
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
+        
+        //Click Log in
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+        
+        //Assert Message
+        String loginMessage = driver.findElement(By.id("action-confirmation")).getText();
+        Assert.assertEquals(loginMessage, "Welcome Back, admin");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        //Close browser
+        driver.close();
+    }
 
 }
