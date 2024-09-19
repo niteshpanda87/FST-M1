55d8db945e04c4c38ab614e60ca7c6c9f678479d
niteshpanda87 <nitesh.panda@ibm.com>
2024-09-19 09:29:24 +0530

    Add files via upload

--- Java/Activities/Activity14.java
+++ Java/Activities/Activity14.java
@@ -1,5 +1,11 @@
-package com.ibm.fst_m1_java;
+import java.io.File;
+import java.io.IOException;
 
 public class Activity14 {
+    public static void main (String[] args) throws IOException{
+        File file = new File("src/Java Activities/newText.txt");
+        boolean fstatus = file.createNewFile();
+        
+    }
 
 }
